5bd5d25ce7611be59338bea02bec6ebd2c9c379b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-06-12 01:57:11 +0700

    test(ci): add coveralls integration
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- test/config.php
+++ test/config.php
@@ -1,12 +1,17 @@
 <?php
+/**
+ * @link https://kahlan.github.io/docs/config-file.html
+ */
 
 /** @var Kahlan\Cli\CommandLine $cli */
 
 use Kahlan\Filter\Filters;
+use Kahlan\Reporter\Coverage\Exporter\Coveralls;
 
 $cli = $this->commandLine();
-$cli->option('spec', 'default', ['test']);
+$cli->option('coverage', 'default', 3);
 $cli->option('grep', 'default', ['*.test.php']);
+$cli->option('spec', 'default', ['test']);
 
 Filters::apply($this, 'namespaces', function($next) {
     $this->autoloader()->addPsr4('Projek\\ContainerStub\\', __DIR__ . '/stub/');

c70361aef7329771e812e4fb4a9187ffe95fa268
Fery Wardiyanto <ferywardiyanto@gmail.com>
2020-07-11 03:42:37 +0700

    chore: move constructor & update some codes
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- src/Container/AbstractContainerAware.php
+++ src/Container/AbstractContainerAware.php
@@ -13,14 +13,4 @@ namespace Projek\Container;
 abstract class AbstractContainerAware implements ContainerAwareInterface
 {
     use ContainerAware;
-
-    /**
-     * Create new instance.
-     *
-     * @param ContainerInterface $container
-     */
-    final public function __construct(ContainerInterface $container)
-    {
-        $this->setContainer($container);
-    }
 }
